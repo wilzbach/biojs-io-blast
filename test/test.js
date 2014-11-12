2794b78e5c1f382bc5a28d116b257a8e892175a9
Sebastian Wilzbach <sebi@wilzbach.me>
2014-11-12 06:17:27 +0100

    applied some EBI XML specific hacks

--- test/test.js
+++ test/test.js
@@ -16,13 +16,11 @@ var equal = assert.equal;
 // requires your main app (specified in index.js)
 var blast = require("../");
 
-
-describe('biojs-io-blast module', function(){
+describe('NCBI format', function(){
   var result,hits;
   before(function(){
-    var str = require("fs").readFileSync(__dirname + "/dummy/ebi.xml");
+    var str = require("fs").readFileSync(__dirname + "/dummy/single.xml");
     result = blast.parse(str);
-    console.log(result);
     hits = result.iterations[0].hits;
   })
   it('basic equality', function(){
@@ -48,9 +46,44 @@ describe('biojs-io-blast module', function(){
     equal(hits[26].accession, 139969);
     equal(hits[26].hsps.length, 17);
     equal(hits[26].hsps[0]["bit-score"], 193.356);
-    equal(hits[26].hsps[0]["bit-score"], 193.356);
     equal(hits[26].hsps[0]["evalue"], 6.42773e-47);
     equal(hits[26].hsps[0]["hit-to"], 258);
     equal(hits[26].hsps[0]["qseq"], "EQEIVQKRTFTKWINSHLAKRKPPMVVDDLFEDMKDGVKLLALLEVLSGQKLPCEQGRRMKRIHAVANIGTALKFLEGRKIKLVNINSTDIADGRPSIVLGLMWTIILYFQIEELTSNLPQLQSLSSSASSVDSIVSSETPSPPSKRKVTTKIQGNAKKALLKWVQYTAGKQTGIEVKDFGKSWRSGVAFHSVIHAIRPELVDLETVKG-RSNRENLEDAFTIAETELGIPRLLDPEDVDVDKPDEKSIMTYVAQFLKHYPDIHNASTDGQEDDEILPGFP");
+  });
+});
+
+describe('EBI format', function(){
+  var result,hits;
+  before(function(){
+    var str = require("fs").readFileSync(__dirname + "/dummy/ebi.xml");
+    result = blast.parse(str);
+    hits = result.iterations[0].hits;
+  })
+  it('basic equality', function(){
+    equal(result.program, "blastp");
+    equal(result.param.matrix, "BLOSUM62");
+  });
+
+  it('nr. of iterations', function(){
+    equal(result.iterations.length, 1);
+    equal(result.iterations[0]["query-len"], 3418);
+    equal(result.iterations[0].stat["db-num"], 87121513);
+    equal(hits.length, 50);
+  });
+
+  it('last hit', function(){
+    equal(hits[49].len, 1644);
+    equal(hits[49].hsps[0]["hit-from"], 1);
+    equal(hits[49].hsps[0]["align-len"], 1646);
+    equal(hits[49].hsps[0]["gaps"], 0);
+  });
+
+  it('check a specific hit', function(){
+    equal(hits[26].accession, "W5PNG4");
+    equal(hits[26].hsps.length, 1);
+    equal(hits[26].hsps[0]["bit-score"], 4399.0);
+    equal(hits[26].hsps[0]["evalue"], 0.0);
+    equal(hits[26].hsps[0]["hit-to"], 3369);
+    equal(hits[26].hsps[0]["qseq"].substring(0,20), "MPIGSKERPTFFEIFKTRCN");
   });
 });
